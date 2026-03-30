3a400e4266582cadd8dfe9abb732b11631526649
kamrankamrankhan <kamrankamrankhan825@gmail.com>
2026-03-30 22:56:35 +0000

    Add BlogPoster component: Latest blogs banner on homepage header
    Features:
    - New BlogPoster component displaying 3 latest blog posts
    - Poster-style layout with large featured blog (left, 2/3 width)
    - Secondary blogs stacked on right (1/3 width)
    - Hover effects with image zoom and title color change
    - Date display with clock icon
    - 'Featured' badge on main blog
    - Animated marquee ticker at bottom
    - Responsive design for mobile and desktop
    - Links to individual blog posts
    
    Layout:
    - Featured blog: Large image with gradient overlay
    - Secondary blogs: Stacked cards with same styling
    - View All link to /blogs page
    - Smooth hover animations
    
    Added to HomePage right after Hero section for maximum visibility

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,13 @@ import Footer from '../sections/Footer';
 import FeaturedArticle from '../sections/FeaturedArticle';
 import BlogsSection from '../sections/BlogsSection';
 import NewsSection from '../sections/NewsSection';
+import BlogPoster from '../sections/BlogPoster';
 
 export default function HomePage() {
   return (
     <main className="relative w-full min-h-screen bg-void-black overflow-x-hidden">
       <Hero />
+      <BlogPoster />
       <AlbumCube />
       <ParallaxGallery />
       <NewsSection />
